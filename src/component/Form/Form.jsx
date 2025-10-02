ae500a10e84cfcd8d59ef6afaac0bee5b076ea9c
Max web2025-4 <denderys32@gmail.com>
2025-10-02 16:19:23 +0300

    6 commit

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -3,8 +3,21 @@ import './Form.css'
 
 const Form = () => {
     return (
-        <div>
-            Form
+        <div className="form" >
+            <h3>Введіть ваші дані</h3>
+            <input className={"input"}
+                   type={"text"}
+                   placeholder={'Місто'}
+            />
+            
+            <input className={"input"} 
+                   type={"text"}
+                   placeholder={'Вулиця'}
+            />
+            <select className={"select"}>
+                <option value={"physical"}>Фіз.лице</option>
+                <option value={"legal"}>Юр.Лице</option>
+            </select>
         </div>
     );
 };
