b613197e5869f40e347d89d1644b2d8332d69b22
Max web2025-4 <denderys32@gmail.com>
2025-10-07 01:49:02 +0300

    6 commit

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -1,61 +1,65 @@
-import React, {useEffect} from 'react';
-import './Form.css'
-import {useTelegram} from "../../hooks/useTelegram";
+import React, { useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
     const [country, setCountry] = useState('');
     const [street, setStreet] = useState('');
     const [subject, setSubject] = useState('physical');
-    const {tg} = useTelegram()
+    const { tg } = useTelegram();
 
     useEffect(() => {
-    tg.MainButton.setParams({
-        text:'Відправити дані'
-    })
-    },[])
+        tg.MainButton.setParams({
+            text: 'Відправити дані'
+        });
+    }, [tg]);
 
     useEffect(() => {
-       if (!street || !country) {
-           tg.MainButton.hide()
-       }else {
-           tg.MainButton.show()
-       }
-    }, [country , street]);
+        if (!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street, tg]);
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value);
-
     }
 
-    const onChangeStreer = (e) => {
+    const onChangeStreet = (e) => {
         setStreet(e.target.value);
-
     }
 
     const onChangeSubject = (e) => {
         setSubject(e.target.value);
-
     }
 
     return (
-        <div className="form" >
+        <div className="form">
             <h3>Введіть ваші дані</h3>
-            <input className={"input"}
-                   type={"text"}
-                   placeholder={'Місто'}
-                   value={country}
-                   onChange={onChangeCountry}
+            <input
+                className="input"
+                type="text"
+                placeholder="Місто"
+                value={country}
+                onChange={onChangeCountry}
             />
-            
-            <input className={"input"} 
-                   type={"text"}
-                   placeholder={'Вулиця'}
-                   value={street}
-                   onChange={onChangeStreer}
+
+            <input
+                className="input"
+                type="text"
+                placeholder="Вулиця"
+                value={street}
+                onChange={onChangeStreet}
             />
-            <select value={subject} onChange={onChangeSubject} className={"select"}>
-                <option value={"physical"}>Фіз.лице</option>
-                <option value={"legal"}>Юр.Лице</option>
+
+            <select
+                value={subject}
+                onChange={onChangeSubject}
+                className="select"
+            >
+                <option value="physical">Фіз. особа</option>
+                <option value="legal">Юр. особа</option>
             </select>
         </div>
     );
