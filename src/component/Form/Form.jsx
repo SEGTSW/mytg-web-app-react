a88c493a13ae035a828d6be90f92e988def9f967
Max web2025-4 <denderys32@gmail.com>
2025-10-07 01:06:53 +0300

    6 commit

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -2,19 +2,42 @@ import React from 'react';
 import './Form.css'
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value);
+
+    }
+
+    const onChangeStreer = (e) => {
+        setStreet(e.target.value);
+
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value);
+
+    }
+
     return (
         <div className="form" >
             <h3>Введіть ваші дані</h3>
             <input className={"input"}
                    type={"text"}
                    placeholder={'Місто'}
+                   value={country}
+                   onChange={onChangeCountry}
             />
             
             <input className={"input"} 
                    type={"text"}
                    placeholder={'Вулиця'}
+                   value={street}
+                   onChange={onChangeStreer}
             />
-            <select className={"select"}>
+            <select value={subject} onChange={onChangeSubject} className={"select"}>
                 <option value={"physical"}>Фіз.лице</option>
                 <option value={"legal"}>Юр.Лице</option>
             </select>
